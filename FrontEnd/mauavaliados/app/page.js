5910828b6b14fe6be2996d0a60edd7478a631f1c
RivianSoldier <bilzims.sg@gmail.com>
2024-10-09 19:40:04 -0300

    criacao projeto next

--- FrontEnd/mauavaliados/app/page.js
+++ FrontEnd/mauavaliados/app/page.js
@@ -1,101 +1,97 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import "./globals.css";
+import Link from "next/link";
+import Navbar from "@/components/navbar";
 import Image from "next/image";
+// import logo from "../public/logo_mauavaliados_img.png";
+import logo from "../public/logo_principal.svg";
+import the_witcher from "../public/the_witcher.png";
+import super_smash from "../public/super_smash.png";
+import { MdGames } from "react-icons/md";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    //   <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    //   </main>
+    //   <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+    // </div>
+    // <main className="min-h-screen bg-[#373737] relative">
+    // #8C00FF
+    // #FFAE00
+    // #E1005E
+    <main className="min-h-screen  relative">
+      <SignedIn>
+        <Navbar>
+          <Image
+            src={logo}
+            width={44}
+            height={44}
+            className="cursor-pointer transition ease-in-out hover:scale-105 hover:rotate-6"
+          />
+          <div className="flex items-center justify-around w-2/4">
+            <Link
+              className="relative after:bg-[#FFAE00] after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-500  after:rounded-lg hover:scale-110 p-1 font-bold "
+              href={"/"}
+            >
+              Pesquisar
+            </Link>
+            <Link
+              className="relative after:bg-[#FFAE00] after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-500  after:rounded-lg hover:scale-110 p-1 font-bold"
+              href={"/"}
+            >
+              Minhas Reviews
+            </Link>
+            <Link
+              className="relative after:bg-[#FFAE00] after:absolute after:h-1 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-500  after:rounded-lg hover:scale-110 p-1 font-bold"
+              href={"/"}
+            >
+              Listas
+            </Link>
+          </div>
+          <UserButton
+            appearance={{
+              elements: {
+                avatarBox: "w-11 h-11",
+              },
+            }}
+          />
+        </Navbar>
+        <Image src={the_witcher} className="" />
+        <div className="min-w-max">
+          <div className="flex flex-col items-start justify-start h-screen pl-12 pt-12">
+            <h1 className="text-3xl font-bold pb-4">Lançamentos</h1>
+            <div className="flex w-[230px] h-[346px] bg-white rounded-sm cursor-pointer drop-shadow-xl overflow-hidden">
+              <Image
+                src={super_smash}
+                className="h-auto w-auto object-cover transition-all duration-500 hover:scale-[1.03] rounded-sm"
+              />
+              <TooltipProvider>
+                <Tooltip className="static">
+                  <TooltipTrigger className="absolute right-1 top-1 group">
+                    <MdGames
+                      size={"1.7em"}
+                      className="flex cursor-pointer transition ease-in-out hover:scale-110 hover:rotate-90 hover:text-[#595959]"
+                    />
+                  </TooltipTrigger>
+                  <TooltipContent
+                    side="left"
+                    className="drop-shadow-sm bg-[#8C00FF]/70 text-white"
+                  >
+                    <p>Adicionar à lista</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </SignedIn>
+    </main>
   );
 }
