0f83ffb94f4892e786239f011ffc1d0a8bb5bea4
Kusko99 <fernandaofo120@gmail.com>
2024-10-10 18:19:55 -0300

    Microserviços de Usuário, Create,Get e Update

--- BackEnd/Usuários/main.py
+++ BackEnd/Usuários/main.py
@@ -1,15 +1,16 @@
 from flask import Flask
-from flask_cors import CORS
+from connector import db, init_db
 
 app = Flask(__name__)
-CORS(app)
+
 
 def main():
     #importando e registrando as rotas
     from routes import users_routes
-
+    init_db(app)
     app.register_blueprint(users_routes)
-
-    if __name__ == '__main__':
-        app.run(port=7000, debug=True)
-
+    
+if __name__ == '__main__':
+    main()
+    app.run(port=7000, debug=True)
+   
--- BackEnd/Usuários/routes.py
+++ BackEnd/Usuários/routes.py
@@ -1,5 +1,32 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from controllers import UserController
 
-users_routes = Blueprint('users_routes', __name__)
+users_routes = Blueprint("users_routes", __name__)
 
+
+@users_routes.route("/users", methods=["POST"])
+def create_user():
+    "Rota para criar um usuário"
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"message": "No input data provided"}), 400
+    data = UserController().create_user(data)
+    return data
+
+
+@users_routes.route("/users/<user_id>", methods=["GET"])
+def get_user_by_id(user_id:str):
+    "Rota para buscar um usuário por ID"
+    data = UserController().get_user_by_id(user_id)
+    return data, 200
+
+@users_routes.route("/users/<user_id>", methods=["PUT"])
+def update_user(user_id:str):
+    "Rota para atualizar um usuário"
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"message": "No input data provided"}), 400
+    data = UserController().update_user(user_id,data)
+    return data
