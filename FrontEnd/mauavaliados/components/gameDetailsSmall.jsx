0dfcc9a5df740d651f4b2525f6f0b411069ec68e
Diogo Carvalho <diogobernardini2003@gmail.com>
2024-10-25 14:31:23 -0300

    Ajustes tela detlahes do jogo pequena

--- FrontEnd/mauavaliados/components/gameDetailsSmall.jsx
+++ FrontEnd/mauavaliados/components/gameDetailsSmall.jsx
@@ -23,23 +23,23 @@ const GameDetailsSmall = ({
   generos,
 }) => {
   return (
-    <div className="flex flex-col mx-5  mt-24 gap-6 items-center justify-center">
-      <div className="flex flex-row w-full justify-between">
-        <div className="flex flex-col bg-[#FFAE00]/80 w-fit p-2 justify-around rounded-sm ">
+    <div className="flex flex-col mx-5  mt-24 gap-4 items-center justify-center">
+      <div className="flex flex-row w-full gap-4">
+        <div className="flex flex-col bg-[#FFAE00]/80 w-full p-2 justify-around rounded-sm ">
           <p className="text-sm font-bold">Usuários</p>
           <div className="flex flex-row items-center justify-between w-full gap-4">
             <p className="font-bold text-2xl">{usuarios}</p>
             <FaTrophy size={"1.5rem"} />
           </div>
         </div>
-        <div className="flex flex-col bg-[#8c00ff]/50 w-fit p-2 justify-around rounded-sm ">
+        <div className="flex flex-col bg-[#8c00ff]/50 w-full p-2 justify-around rounded-sm ">
           <p className="text-sm font-bold">IGDB</p>
           <div className="flex flex-row items-center justify-between w-full gap-4">
             <p className="font-bold text-2xl">{igdb}</p>
             <FaTrophy size={"1.5rem"} />
           </div>
         </div>
-        <div className="flex flex-col bg-[#8c00ff]/50 w-fit p-2 justify-around rounded-sm ">
+        <div className="flex flex-col bg-[#8c00ff]/50 w-full p-2 justify-around rounded-sm ">
           <p className="text-sm font-bold">Crítica</p>
           <div className="flex flex-row items-center justify-between w-full gap-4">
             <p className="font-bold text-2xl">{critica}</p>
@@ -51,7 +51,7 @@ const GameDetailsSmall = ({
       <div className="flex flex-row w-full gap-2 justify-between">
         <div className="flex flex-col justify-start gap-2">
           <p className="text-2xl font-bold text-wrap">{title}</p>
-          <div className="flex flex-col justify-start">
+          <div className="flex flex-col justify-start pt-6">
             <p className="font-semibold">Data de lançamento: </p>
             <p className="text-sm">{lancamento}</p>
           </div>
@@ -83,7 +83,7 @@ const GameDetailsSmall = ({
           >
             <Button
               size="sm"
-              className="bg-[#2d2d2d] text-[#FFAE00] mt-1 py-0 rounded-sm hover:bg-[#FFAE00] hover:text-black font-bold gap-2 w-full"
+              className="bg-[#2d2d2d] text-[#FFAE00] mt-1 py-4 rounded-sm hover:bg-[#FFAE00] hover:text-black font-bold gap-2 w-full"
             >
               <FaPlay />
               Trailer
@@ -95,7 +95,7 @@ const GameDetailsSmall = ({
           </Button>
         </div>
       </div>
-      <div className="flex flex-col gap-2 -mt-10">
+      <div className="flex flex-col gap-2">
         <p className="font-semibold w-full">Descrição:</p>
         <p>{descricao}</p>
       </div>
