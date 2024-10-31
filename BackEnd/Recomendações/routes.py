6b572648c89033dd527d6a9541ff1716cd52975b
LTMM51 <luthiagomm@gmail.com>
2024-10-30 22:02:25 -0300

    criação da rota de recomendação

--- BackEnd/Recomendações/routes.py
+++ BackEnd/Recomendações/routes.py
@@ -3,3 +3,9 @@ from controllers import RecomendaController
 
 recomendacao_routes = Blueprint('recomendacao_routes', __name__)
 
+@recomendacao_routes.route("/recomendacao/<user_id>", methods=["GET"])
+def get_recomendacao_by_id(user_id:str):
+    "Rota para buscar uma recomendação por ID do usuario"
+    data = RecomendaController().get_recomendacao_by_id(user_id)
+    return data, 200
+
